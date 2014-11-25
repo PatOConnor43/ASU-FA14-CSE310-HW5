6f475ac67a3c3df9877df9fe6e0189f7980e9cb5
Patrick O'Connor <patoconnor43@gmail.com>
2014-11-25 16:54:11 -0700

    Update so now edges are beautiful

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ASU-FA14-CSE310-HW5
 ===================
 
 Hey guys I just wanted to put this up in case I end up breaking something, but also so you could take a look at what I have so far. You can input the input.txt file through terminal with 'python maxprofit.py input.txt'. I don't know if that works for Windows but it does for Linux.
+
+
+###Commit: Update so now edges are beautiful###
+Okay so I finally got edges to work really nicely. At the end it prints the edges in an unordered form. If you would like to see them in an ordered form then add 'from collections import OrderedDict' at the top and change the definition of edges (line 44) to 'OrderedDict()' instead of '{}'. I hope I'm making progress in the right direction.
--- maxprofit.py
+++ maxprofit.py
@@ -2,6 +2,7 @@
 import sys
 import re
 import string
+#from collections import OrderedDict
 
 #So this function starts to build a graph for you. It is meant to add path by
 #path until the while loop that calls it finishes. The graph is a dict.
@@ -12,6 +13,16 @@ def makeGraph(starting_node, ending_node, profit, time, aGraph):
         aGraph[starting_node].append([ending_node, profit, time])
         
 
+#This searches through each vertex in verticies to find one with a matching
+#name and it returns the index in verticies where it was found
+def findEnd(name_of_node, list_of_vert):
+    
+    for i in range(len(list_of_vert)):
+        if name_of_node == list_of_vert[i][0]:
+            return i
+
+
+
 #Read input file
 inFile = sys.argv[1]
 lines = open(inFile,'r').readlines()
@@ -21,49 +32,43 @@ lines = open(inFile,'r').readlines()
 nVert = int(re.sub("[^0-9]", "", lines[0]))
 
 #Some vertex information lists --WILL PROBABLY BE CHANGED--
-verticies_name = []
-verticies_profit = []
-verticies = [verticies_name, verticies_profit]
+#verticies_name = []
+#verticies_profit = []
+verticies = []
 
 #Same as verticies but with edges
-nEdges = int(re.sub("[^0-9]", "", lines[nVert+1]))
+nEdge = int(re.sub("[^0-9]", "", lines[nVert+1]))
 edge_start = []
 edge_end = []
 edge_time =[]
-edges = [edge_start, edge_end, edge_time]
+edges = {}
 
 #Making an empty dict for graph
 graph = {}
 
-#This loop appends to the vertex lists using regular expressions
-for i in xrange(1, nVert+1):
-    verticies_name.append(re.sub("[^a-zA-Z]", "", lines[i]))
-    verticies_profit.append(re.sub("[^0-9]", "", lines[i]))
+#This loop adds Verticies to the Verticies list in the form V = [Vertex_Name, Profit]
+#So this list will look like Verticies = [V1, V2,..., Vn]
+#To be clear, each V is a ---LIST--- so I will not be putting another set of [] around them
+for i in range(1, nVert+1):
+    verticies.append([re.sub("[^a-zA-Z]", "", lines[i]), re.sub("[^0-9]", "", lines[i])])
 
-#This loop adds to edges using regular expressions
-for j in xrange(nVert+2, nVert+nEdges+2):
-    edge_start.append(lines[j].split()[0])
-    edge_end.append(lines[j].split()[1])
-    edge_time.append(lines[j].split()[2])
-
-
-#Some loop variables
-indexi = 0
-indexj = 0
+#This loop will create a dict of edges in the form edges['edge_name'] = [[V1],[V2],time]
+#An example of the entire dict would be edges = {'V1V2':[V1, V2, time], ..., 'Vn-1Vn':[Vn-1, Vn, time]}
+#This holds as long as the Verticies are actually connected, otherwise try anotherr pair
+j = nVert+2
 go = True
-
-#This is where the magic happens... eventually. This loop is a liar. Currently it works,
-#but the profit values are incorrect. Working on remedy on whiteboard. If you run it,
-#it kind of looks nice though
-for i in xrange(len(verticies_name)):
-    while verticies_name[i] == edge_start[indexi] and go:
-        makeGraph(edge_start[indexi], edge_end[indexi],verticies_profit[i] , edge_time[indexi], graph)
-        if indexi < len(edge_start)-1:
-            indexi += 1
+for i in range(len(verticies)):
+    while lines[j].split()[0] == verticies[i][0] and go:
+        findMe = lines[j].split()[1]
+        index = findEnd(findMe, verticies)
+        edges[lines[j].split()[0]+lines[j].split()[1]] = [verticies[i], verticies[index], lines[j].split()[2]]
+        if j < len(lines)-1:
+            j+=1
         else:
             go = False
-   
-print graph
+print edges
+
+
 
 #These are just for checking values
 """
